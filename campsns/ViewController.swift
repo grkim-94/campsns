8a6a09f1e6261fe9f36208718fe04ea5a70c2d6f
grkim-94 <grmkim94@gmail.com>
2020-07-04 22:49:39 +0900

    - iOS target 11.0 으로 변경 - SwiftUI 제거 - SceneDelegate 제거

---
+++ campsns/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  campsns
+//
+//  Created by 김가람 on 2020/07/04.
+//  Copyright © 2020 김가람. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
